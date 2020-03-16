caa57fcc4c1d16d3aeb295c5feac6806e44a6826
yukihamu1206 <iida.yuki.infratop@gmail.com>
2020-03-16 12:56:22 +0900

    user tableとtweet table作った

---
+++ app/Models/Tweet.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tweet extends Model
+{
+    protected $fillable = [
+        'text'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
