c0c37860be8deaa38cd77aad5e74a6b01c184dee
yukihamu1206 <iida.yuki.infratop@gmail.com>
2020-03-18 20:11:16 +0900

    s3変更

--- app/Models/Tweet.php
+++ app/Models/Tweet.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Tweet extends Model
 {
@@ -24,7 +25,7 @@ class Tweet extends Model
             $elm = [
                 'text' => $tweet->text,
                 'created_at' => $tweet->created_at->format('Y/m/d H:i'),
-                'profile_image' => $tweet->user->profile_image ? $tweet->user->profile_image : 'noimage.jpg',
+                'profile_image' => Storage::disk('s3')->url($tweet->user->profile_image ? $tweet->user->profile_image : 'noimage.jpg'),
                 'name' => $tweet->user->name,
                 'screen_name' => $tweet->user->screen_name,
                 'user_id' => $tweet->user->id,
