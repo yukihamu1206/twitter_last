132fbe3ab8c6ffa2e7a65a8b99c88f5b6b1651d0
yukihamu1206 <iida.yuki.infratop@gmail.com>
2020-03-18 19:58:19 +0900

    修正

--- app/Http/Controllers/TweetsController.php
+++ app/Http/Controllers/TweetsController.php
@@ -4,8 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Tweet;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Storage;
 use Illuminate\Pagination\LengthAwarePaginator;
 
 class TweetsController extends Controller
@@ -13,22 +11,27 @@ class TweetsController extends Controller
     public function create()
     {
         $token = Auth()->user()->api_token;
-        return view('tweets.create',['token' => $token]);
+        return view('tweets.create', ['token' => $token]);
     }
 
-    public function index(Request $request,Tweet $tweet)
+    /**
+     * @param  Request  $request
+     * @param  Tweet  $tweet
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function index(Request $request, Tweet $tweet)
     {
-         $lists = $tweet->getTimeline();
+        $lists = $tweet->getTimeline();
 
         $lists = new LengthAwarePaginator(
             $lists,
             count(Tweet::all()),
             5,
-           $request->page,
+            $request->page,
             array('path' => $request->url())
-       );
+        );
 
-        return view('tweets.index',['lists' => $lists]);
+        return view('tweets.index', ['lists' => $lists]);
 
     }
 }
--- app/Models/Tweet.php
+++ app/Models/Tweet.php
@@ -22,13 +22,13 @@ class Tweet extends Model
         $timeline = [];
         foreach ($tweets as $tweet) {
             $elm = [
-                'text'          => $tweet->text,
-                'created_at'    => $tweet->created_at->format('Y/m/d H:i'),
+                'text' => $tweet->text,
+                'created_at' => $tweet->created_at->format('Y/m/d H:i'),
                 'profile_image' => $tweet->user->profile_image ? $tweet->user->profile_image : 'noimage.jpg',
-                'name'          => $tweet->user->name,
-                'screen_name'   => $tweet->user->screen_name,
-                'user_id'       => $tweet->user->id,
-                'tweet_id'      => $tweet->id,
+                'name' => $tweet->user->name,
+                'screen_name' => $tweet->user->screen_name,
+                'user_id' => $tweet->user->id,
+                'tweet_id' => $tweet->id,
             ];
 
             $timeline[] = $elm;
