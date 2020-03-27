2a57a8c5be4ee19a6336bce93ea2b4f710d79ef2
yukihamu1206 <iida.yuki.infratop@gmail.com>
2020-03-27 15:01:13 +0900

    aws sdk for php

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,181 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\TweetRequest;
+use App\Http\Requests\UserRequest;
+use App\Models\Favorite;
+use App\Models\Tweet;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\Validator;
+
+
+
+class ApiController extends Controller
+{
+
+    /**
+     *TweetRequestでvalidationチェックして、jsonを返す
+     *
+     * @param  TweetRequest  $request
+     *
+     * @return JsonResponse
+     */
+    public function store(TweetRequest $request)
+    {
+
+
+        $tweet = new Tweet;
+        $tweet->text = $request->text;
+        $tweet->user_id = Auth()->id();
+
+        #validatorを取得する
+        $validator = $request->getValidator();
+
+        if ($validator->fails()) {
+            return response()->json(
+                [
+                    'result' => false,
+                    'errors' => $validator->errors()
+                ],
+                200,
+                [],
+                JSON_UNESCAPED_UNICODE
+            );
+        }
+
+        $tweet->save();
+
+        return response()->json(
+            [
+                'result' => true,
+                'tweet' => $tweet
+            ],
+            200,
+            [],
+            JSON_UNESCAPED_UNICODE
+        );
+    }
+
+    /**
+     * ツイートを編集
+     *
+     * @param  TweetRequest  $request
+     * @param  Tweet  $tweet
+     * @return JsonResponse
+     */
+    public function update(TweetRequest $request, Tweet $tweet)
+    {
+
+        $tweet->text = $request->text;
+        $tweet->user_id = Auth()->id();
+
+        $validator = $request->getValidator();
+
+        if ($validator->fails()) {
+            return response()->json(
+                [
+                    'result' => false,
+                    'errors' => $validator->errors()
+                ],
+                200,
+                [],
+                JSON_UNESCAPED_UNICODE
+            );
+        }
+
+        $tweet->updateTweet($tweet->user_id, $tweet->id);
+
+        return response()->json(
+            [
+                'result' => true
+            ],
+            200,
+            [],
+            JSON_UNESCAPED_UNICODE
+        );
+
+    }
+
+    /**
+     * ツイートを削除
+     *
+     * @param  Tweet  $tweet
+     *
+     * @return JsonResponse
+     */
+    public function destroy(Tweet $tweet)
+    {
+        $user = Auth()->user();
+        $tweet->deleteTweet($user->id, $tweet->id);
+
+        return response()->json([
+            'result' => true
+        ]);
+
+
+    }
+
+    /**
+     *いいねをする
+     *
+     * @param  Request  $request
+     *
+     * @param  Favorite  $favorite
+     *
+     * @return JsonResponse
+     */
+    public function favorite(Request $request, Favorite $favorite)
+    {
+
+        $user_id = Auth::id();
+        $tweet_id = $request->tweet_id;
+        $is_Favorite = $favorite->isFavorite($user_id, $tweet_id);
+
+
+        if (!$is_Favorite) {
+            $favorite->storeFavorite($user_id, $tweet_id);
+            $favorite_count = $favorite->where('tweet_id', $tweet_id)->count();
+            $user_favorite_id = $favorite->where('user_id', $user_id)->where('tweet_id', $tweet_id)->first()->id;
+
+
+            return response()->json([
+                'result' => true,
+                'favorite_count' => $favorite_count,
+                'user_favorite_id' => $user_favorite_id
+            ]);
+        }
+    }
+
+
+    /**
+     * いいね削除
+     *
+     * @param  Favorite  $favorite
+     *
+     * @return JsonResponse
+     */
+    public function deleteFavorite(Favorite $favorite)
+    {
+        $user_id = Auth()->id();
+        $tweet_id = $favorite->tweet_id;
+        $favorite_id = $favorite->id;
+        $is_Favorite = $favorite->isFavorite($user_id, $tweet_id);
+
+
+        if ($is_Favorite) {
+            $favorite->deleteFavorite($favorite_id);
+            $favorite_count = $favorite->where('tweet_id', $tweet_id)->count();
+
+            return response()->json([
+                'result' => true,
+                'favorite_count' => $favorite_count
+            ]);
+        }
+    }
+
+}
---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\UserRequest;
+use App\Models\Tweet;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Aws\S3\S3Client;
+use Illuminate\Pagination\LengthAwarePaginator;
+
+
+class UsersController extends Controller
+{
+
+
+    /**
+     *user詳細表示
+     *
+     * @param  User  $user
+     * @param  Tweet  $tweet
+     * @param  Request  $request
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function show(User $user, Tweet $tweet, Request $request)
+    {
+
+        $login_user = Auth()->user();
+        $lists = $tweet->getUserTweet($user->id);
+        $tweet_count = $tweet->getTweetCount($user->id);
+
+//        create a S3Client
+        $s3 = new S3Client([
+            'version' => 'latest',
+            'region' => config('app.region'),
+        ]);
+
+        $profile_image = $s3->getObjectUrl(
+            config('app.bucket'),
+            $user->profile_image,
+        );
+
+
+        $lists = new LengthAwarePaginator(
+            $lists,
+            count(Tweet::all()),
+            5,
+            $request->page,
+            array('path' => $request->url())
+        );
+
+        return view('users.show',[
+            'login_user' => $login_user,
+            'name' => $user->name,
+            'screen_name' => $user->screen_name,
+            'user_id' => $user->id,
+            'lists' => $lists,
+            'tweet_count' => $tweet_count,
+            'profile_image' => $profile_image
+        ]);
+
+
+
+
+
+    }
+
+
+    /**
+     * ユーザー情報編集
+     *
+     * @param  User  $user
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector|\Illuminate\View\View
+     */
+    public function edit(User $user)
+    {
+        if($user->id === Auth()->id()) {
+            return view('users.edit', [
+                'user_id' => $user->id,
+                'screen_name' => $user->screen_name,
+                'name' => $user->name,
+                'email' => $user->email,
+                'profile_image' => $user->profile_image
+            ]);
+        }else{
+            return redirect('/');
+        }
+
+    }
+
+/**
+* @param  UserRequest  $request
+* @param  User  $user
+* @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+*/
+    public function update(UserRequest $request,User $user)
+    {
+        $data = $request->all();
+
+
+        $user->updateProfile($data);
+
+        return redirect('user/'.$user->id);
+
+    }
+}
